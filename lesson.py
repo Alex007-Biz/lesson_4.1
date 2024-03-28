c828a86fc68df1d00ea6621d465e1246083c938f
Alex007-Biz <bizukin@yandex.ru>
2024-03-28 08:17:46 +0300

    new lesson

---
+++ lesson.py
@@ -0,0 +1,18 @@
+class Warrior ():
+    def init(self, name, power, endurance, hair_color):
+        self.name = name
+        self.power = power
+        self.endurance = endurance
+        self.hair_color = hair_color
+
+    def eat(self):
+        print(f"{self.name} сел кушать")
+        self.power = + 1
+    def walk(self):
+        print(f"{self.name} гуляет")
+
+    def info(self):
+        print(f"имя воина - {self.name}")
+        print(f"цвет волос воина - {self.hair_color}")
+        print(f"сила воина - {self.power}")
+        print(f"выносливость воина - {self.endurance}")
--- main.py
+++ main.py
@@ -1,1 +1,30 @@
-print("Hello")
+
+class Task():
+    def __init__(self, description, deadline, status):
+        self.description = description
+        self.deadline = deadline
+        self.status = status
+        self.tasks = []
+
+    def add_task(self, description, deadline):
+       new_task = Task(description, deadline)
+       self.tasks.append(new_task)
+
+    def mark_task_as_done(self, description):
+        for task in self.tasks:
+            if task.description == description:
+                task.status = True
+                break
+
+    def print_current_tasks(self):
+        print("Current tasks:")
+        for task in self.tasks:
+            if not task.status:
+                print(f"Description: {task.description}, Deadline: {task.deadline}, Status: {'Done' if task.status else 'Not Done'}")
+
+task = Task()
+    task.add_task("Complete project report", "2022-10-15")
+    task.add_task("Buy groceries", "2022-10-20")
+    task.add_task("Exercise for 30 minutes", "2022-10-25")
+task.mark_task_as_done("Buy groceries")
+task.print_current_tasks()
