a29950b995da0fa3ea3db528681e704520437972
Alex007-Biz <bizukin@yandex.ru>
2024-03-28 08:46:07 +0300

    bank

--- lesson.py
+++ lesson.py
@@ -1,5 +1,5 @@
-class Warrior ():
-    def init(self, name, power, endurance, hair_color):
+class Warrior():
+    def __init__(self,  name, power, endurance, hair_color):
         self.name = name
         self.power = power
         self.endurance = endurance
@@ -7,7 +7,8 @@ class Warrior ():
 
     def eat(self):
         print(f"{self.name} сел кушать")
-        self.power = + 1
+        self.power += 1
+
     def walk(self):
         print(f"{self.name} гуляет")
 
@@ -16,3 +17,20 @@ class Warrior ():
         print(f"цвет волос воина - {self.hair_color}")
         print(f"сила воина - {self.power}")
         print(f"выносливость воина - {self.endurance}")
+
+
+war2 = Warrior("Stepa",7,20,"black")
+war1 = Warrior("Jack",8,20,"red")
+
+print(war2.name)
+print(war2.power)
+print(war2.endurance)
+print(war2.hair_color)
+war2.eat()
+war2.eat()
+war2.eat()
+print(war2.power)
+
+
+
+
